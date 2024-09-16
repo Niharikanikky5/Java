d6db22c08cad21b2c23c213910f235e32cf860eb
NIharika <nikkydot2014@gmail.com>
2024-09-16 19:30:10 +0530

    OPERATORS

---
+++ src/sept16092024/Arthmeticoperators.java
@@ -0,0 +1,15 @@
+package sept16092024;
+
+public class Arthmeticoperators {
+        public static void main(String[] args) {
+            // Arithmetic Operator
+// +,-,/,*
+            int a = 10;
+            int b = 2;
+            System.out.println(a+b); // + -> binary - 2 operands
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+        }
+
+}
---
+++ src/sept16092024/Logicaloperators.java
@@ -0,0 +1,23 @@
+package sept16092024;
+
+public class Logicaloperators {
+    public static void main(String[] Args){
+
+
+                // && operator
+                System.out.println((3 > 2) && (9 > 2));  // true
+                System.out.println((5 > 1) && (8 < 5));  // false
+
+                // || operator
+                System.out.println((7 < 3) || (6 > 5));  // true
+                System.out.println((9 > 3) || (9 < 5));  // true
+                System.out.println((8 < 3) || (9 < 5));  // false
+
+                // ! operator
+                System.out.println(!(4 == 3));  // true
+                System.out.println(!(6 > 3));  // false
+            }
+        }
+
+
+
