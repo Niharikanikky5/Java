f78e872dc50008bb0098565525b5342e1692e127
Niharikanikky <nikkydot2014@gmail.com>
2024-10-07 13:18:40 +0530

    All the files

---
+++ src/ZPRACTICE/Calculateradius.java
@@ -0,0 +1,12 @@
+package ZPRACTICE;
+
+public class Calculateradius {
+    public static void main(String[] args) {
+        int radius;
+        double pi = 3.14,area;
+        radius =5;
+        System.out.println("enter the radius of the circle");
+        area = pi * radius* radius;
+        System.out.println("Area of circle:"+area);
+    }
+}
