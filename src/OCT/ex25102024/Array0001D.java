69697ef3c4bd7cc2474d075f57bfed4a52602b33
Niharikanikky <nikkydot2014@gmail.com>
2024-10-27 15:09:55 +0530

    Merge remote-tracking branch 'JAVA/Niharika' into Niharika

---
+++ src/OCT/ex25102024/Array0001D.java
@@ -0,0 +1,12 @@
+package OCT.ex25102024;
+
+public class Array0001D {
+    public static void main(String[] args) {
+        //1D
+        int[] marks = new int[5];
+        // Create an Empty array with default values
+        // marks = {0,0,0,0,0}
+        System.out.println(marks[0]);
+
+    }
+}
